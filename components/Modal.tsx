5e641caf21d8d71f3ca381c83e982a959047f7eb
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2022-09-10 11:54:36 -0700

    fix UI

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -65,7 +65,7 @@ const Modal = ({name, username, setName, setUsername, image, setImage, id} : any
                   Edit Profile
                 </h3>
                 <button
-                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                  className="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                   onClick={() => setShowModal(false)}
                 >
                   <span className="text-black h-6 w-6 text-2xl ">
@@ -80,7 +80,7 @@ const Modal = ({name, username, setName, setUsername, image, setImage, id} : any
                   <div className="relative z-0 mb-6 w-full group">
                       <input 
                         defaultValue={username} 
-                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required 
+                        className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required 
                         onChange={(e) => setNewUsername(e.target.value)}
                         />
 
@@ -89,7 +89,7 @@ const Modal = ({name, username, setName, setUsername, image, setImage, id} : any
                   <div className="relative z-0 mb-6 w-full group">
                       <input 
                         defaultValue={name}   
-                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required 
+                        className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required 
                         onChange={(e) => setNewName(e.target.value)}
                         />
                       <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
